8b7388249347cb61b000507cdeb3f4a4fd9ad304
zed <zed@mail.ru>
2019-05-16 23:57:06 +0300

    localized some htmls

--- vshaurme/decorators.py
+++ vshaurme/decorators.py
@@ -9,9 +9,9 @@ def confirm_required(func):
     def decorated_function(*args, **kwargs):
         if not current_user.confirmed:
             message = Markup(
-                'Please confirm your account first.'
-                'Not receive the email?'
-                '<a class="alert-link" href="%s">Resend Confirm Email</a>' %
+                'Пожалйста подтвердите учетную запись. Please confirm your account first.'
+                'Не получили письмо с подтверждением? Not receive the email?'
+                '<a class="alert-link" href="%s">Послать письмо с подтверждением заново. Resend Confirm Email</a>' %
                 url_for('auth.resend_confirm_email'))
             flash(message, 'warning')
             return redirect(url_for('main.index'))
