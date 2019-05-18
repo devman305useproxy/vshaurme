ee26d51d6325b10f184e2a480f6d5f05861d3d9a
zed <zed@mail.ru>
2019-05-18 11:55:43 +0300

    localized forms/main

--- vshaurme/forms/main.py
+++ vshaurme/forms/main.py
@@ -4,15 +4,15 @@ from wtforms.validators import DataRequired, Optional, Length
 
 
 class DescriptionForm(FlaskForm):
-    description = TextAreaField('Description', validators=[Optional(), Length(0, 500)])
-    submit = SubmitField()
+    description = TextAreaField('Описание', validators=[Optional(), Length(0, 500)])
+    submit = SubmitField("Приянть")
 
 
 class TagForm(FlaskForm):
-    tag = StringField('Add Tag (use space to separate)', validators=[Optional(), Length(0, 64)])
-    submit = SubmitField()
+    tag = StringField('Добавьте тег(пробел для разделения) Add Tag (use space to separate)', validators=[Optional(), Length(0, 64)])
+    submit = SubmitField("Добавить")
 
 
 class CommentForm(FlaskForm):
     body = TextAreaField('', validators=[DataRequired()])
-    submit = SubmitField()
+    submit = SubmitField("Отправить")
