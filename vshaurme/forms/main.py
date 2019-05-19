82869baf36bb91cd1ca45d5dd79421d7ab0da8c9
zed <zed@mail.ru>
2019-05-19 20:07:46 +0300

    localization fixes in forms/main

--- vshaurme/forms/main.py
+++ vshaurme/forms/main.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired, Optional, Length
 
 class DescriptionForm(FlaskForm):
     description = TextAreaField('Описание', validators=[Optional(), Length(0, 500)])
-    submit = SubmitField("Приянть")
+    submit = SubmitField("Принять")
 
 
 class TagForm(FlaskForm):
-    tag = StringField('Добавьте тег(пробел для разделения) Add Tag (use space to separate)', validators=[Optional(), Length(0, 64)])
+    tag = StringField('Добавьте тег(пробел для разделения)', validators=[Optional(), Length(0, 64)])
     submit = SubmitField("Добавить")
 
 
