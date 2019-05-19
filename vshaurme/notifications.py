a61153ff4af3494c708c02aa3bafe3bf3455af43
zed <zed@mail.ru>
2019-05-19 20:29:16 +0300

    localized notifications

--- vshaurme/notifications.py
+++ vshaurme/notifications.py
@@ -5,19 +5,19 @@ from vshaurme.models import Notification
 
 
 def push_follow_notification(follower, receiver):
-    message = 'Пользователь подписан на вас. User <a href="%s">%s</a> followed you.' % \
+    message = 'Пользователь <a href="%s">%s</a> подписался на вас.' % \
               (url_for('user.index', username=follower.username), follower.username)
     notification = Notification(message=message, receiver=receiver)
 
 
 def push_comment_notification(photo_id, receiver, page=1):
-    message = '<a href="%s#comments">Под этим фото появился комментарий/ответ. This photo</a> has new comment/reply.' % \
+    message = 'Под <a href="%s#comments">этим</a> фото появился комментарий/ответ.' % \
               (url_for('main.show_photo', photo_id=photo_id, page=page))
     notification = Notification(message=message, receiver=receiver)
 
 
 def push_collect_notification(collector, photo_id, receiver):
-    message = 'Пользователь взял ваше фото в коллекцию. User <a href="%s">%s</a> collected your <a href="%s">photo</a>' % \
+    message = 'Пользователь <a href="%s">%s</a> взял ваше фото <a href="%s">photo</a> в коллекцию.' % \
               (url_for('user.index', username=collector.username),
                collector.username,
                url_for('main.show_photo', photo_id=photo_id))
