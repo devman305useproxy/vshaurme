d4240602007f3b2cee33a14277dad8187917c785
zed <zed@mail.ru>
2019-06-05 23:20:44 +0300

    added loading env vars

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from vshaurme import create_app
 from vshaurme.extensions import db
+from dotenv import load_dotenv
 
+load_dotenv()
 app = create_app()
 db.create_all(app=app)
 app.run(port='5000', host='0.0.0.0')
