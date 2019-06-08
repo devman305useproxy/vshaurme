71e90cba263ad93f25292ce274bd3c8b5c6d369b
alex <zed-chi@mail.ru>
2019-06-08 14:51:22 +0300

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from vshaurme import create_app
 from vshaurme.extensions import db
-from dotenv import load_dotenv
 
-load_dotenv()
+
 app = create_app()
 db.create_all(app=app)
 app.run(port='5000', host='0.0.0.0')
